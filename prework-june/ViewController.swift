6ec6bced8511f0c99600072bbffaf51ef9671663
June.C <juneechen2021@outlook.com>
2025-01-04 16:00:33 -0500

    Initial Commit

---
+++ prework-june/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  prework-june
+//
+//  Created by June C on 1/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
